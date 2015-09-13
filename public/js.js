13aca28a2be1f7bca95db8f91f95b205e5b42fdb
Yair Gonzalez <yairg@redmantech.com>
2015-09-13 16:56:17 -0500

    Changes some styles, fix cross-browser problems with flexbox and adds basic functionality (only supports mexican league atm)

--- public/js.js
+++ public/js.js
@@ -1,19 +1,25 @@
-var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'http%3A%2F%2Fwww.foxsports.com%2Fsoccer%2Fschedule'%20and%20xpath%3D'%2F%2Fsection%5Bcontains(%40class%2C%22wisfb_scrollableContentX%22)%5D'&format=json&diagnostics=true&callback=?"
-var url2 = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'http%3A%2F%2Fwww.foxsports.com%2Fsoccer%2Fschedule'%20and%20xpath%3D'%2F%2Fdiv%5Bcontains(%40class%2C%22wisfb_scheduleGame%22)%5D%2F%2Fspan'&format=json&diagnostics=true&callback=?"
-$.getJSON(url2).done(function (data) {
-	var teamData = data.query.results.span
-	console.log(teamData)
-	for(var i = 0; i<6, teamData[i].indexOf('(') < 0; i++){
-		if(i % 2 == 0){
-			$('.Calendar-local-list').append('<li>'+teamData[i]+'</li>')
+var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'http%3A%2F%2Fwww.espn.com.mx%2Ffutbol%2Ffixtures%3Fleague%3DMEX.1'%20and%20xpath%3D'%2F%2Fdiv%5B%40class%3D%22mod-container%20mod-table%20mod-no-header-footer%22%5D'&format=json&diagnostics=true&callback=?"
+var isTouchDevice = 'ontouchstart' in document.documentElement;
+if(isTouchDevice){
+	$('.Footer-arrow').show()
+}
+$.getJSON(url).done(function (data) {
+	var date = data.query.results.div[0].div.table.tbody.tr[0].td.content
+	var Teamlist = data.query.results.div[0].div.table.tbody.tr;
+	console.log(Teamlist)
+	for(var i = 2; i<Teamlist.length;i++){
+		var it = 1;
+		$('#localList').append('<li>' + Teamlist[i].td[it].a.content + '</li>')
+		$('#visitList').append('<li>' + Teamlist[i].td[3].a.content + '</li>')
+		if(typeof(Teamlist[i].td[0]) != 'object'){
+			$('#scheduleList').append('<li>' + Teamlist[i].td[0] +'</li>')
 		}else{
-			$('.Calendar-visit-list').append('<li>'+teamData[i]+'</li>')
+			$('#scheduleList').append('<li>' + Teamlist[i].td[0].span.content +'</li>')
 		}
+		it++;
 	}
 	$('.Layout-loading').css('display', 'none')
 	$('.Wrapper').css('display', 'block')
+	$('#calendarDay').text(date)
 })
-$.getJSON(url).done(function (data) {
-	var matchDate = data.query.results.section.div.div[0].content
-	$('#calendarDay').text(matchDate)
-})
+
